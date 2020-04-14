dd9fca2b2852bb2023f0a833ce73da2f5bfcc40b
naxinyi <780957235@qq.com>
2020-04-14 15:48:52 +0800

    4.14

---
+++ 第一阶段/vue/test/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>{{name}}</div>
+        <base-son>
+            <p>哈哈哈哈</p>
+        </base-son>
+    </div>
+</template>
+<script>
+
+import BaseSon from "./Son";
+
+export default {
+    data(){
+        return {
+            name:"hahah"
+        }
+    },
+    components:{
+        BaseSon
+    }
+}
+</script>
+<style scoped>
+
+
+</style>
---
+++ 第一阶段/vue/test/Son.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>{{user.age}}</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            user:{
+                name:"oaoa",
+                age:12
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
