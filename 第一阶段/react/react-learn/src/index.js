d9fa06ebc6582a490703ed81a7e652cc6625e96f
naxinyi <780957235@qq.com>
2020-05-01 07:48:16 +0800

    5.1

---
+++ 第一阶段/react/react-learn/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<h1>这是h1标签</h1>, document.getElementById("root"));
