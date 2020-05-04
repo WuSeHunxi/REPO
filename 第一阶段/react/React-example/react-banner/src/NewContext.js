ee7225c408963850f6305e9dd9eeaca486344845
naxinyi <780957235@qq.com>
2020-05-04 18:17:25 +0800

    5.4

--- 第一阶段/react/React-example/react-banner/src/NewContext.js
+++ 第一阶段/react/React-example/react-banner/src/NewContext.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+const ctx = React.createContext();
 
-
-class ChildA extends React.Component{
-    render(){
-        return <div><h2>ChildA</h2></div>
-    }
+class ChildA extends React.Component {
+  static contextType = ctx;
+  render() {
+    return (
+      <div>
+        <h2>ChildA</h2>
+        <p>姓名：{this.context.name}</p>
+        <p>年龄：{this.context.age}</p>
+      </div>
+    );
+  }
 }
 
-
-function ChildB(props){
-    return <div><h1>ChildB</h1></div>
+function ChildB(props) {
+  return (
+    <div>
+      <h1>ChildB</h1>
+      <ctx.Consumer>{(value) => <p>姓名：{value.name}</p>}</ctx.Consumer>
+    </div>
+  );
 }
-
 
 export default class NewContext extends Component {
-    state={
-        
-    }
-    render() {
-        return (
-            <div>
-                <ChildA/>
-                <ChildB/>
-            </div>
-        )
-    }
+  state = {
+    name: "haha",
+    age: 16,
+    sex: "male",
+  };
+  render() {
+    return (
+      <ctx.Provider value={this.state}>
+        <div>
+          <ChildA />
+          <ChildB />
+        </div>
+      </ctx.Provider>
+    );
+  }
 }
